04b8b78df6262268881c3e6953c3cbddfc341f5b
“Ina” <ina.kurets@yandex.by>
2022-07-07 18:14:02 +0300

    feature/JS-3: - add first files

--- js/calculacor.js
+++ js/calculacor.js
@@ -1,16 +1,73 @@
+const fakeInput = document.querySelector("#fakeInput");
+const mainInput = document.querySelector("#mainInput");
+
+
 const insert = (sum) => {
-	document.calculator.textview.value = document.calculator.textview.value + sum;
-}
+	mainInput.value = mainInput.value + sum;
+	if (mainInput.value === "+" || mainInput.value === "*" || mainInput.value === "/" || mainInput.value === "--" || mainInput.value === "00") {
+		mainInput.value = "";
+	}
+	else if (sum === "+" || sum === "*" || sum === "/") {
+		fakeInput.value = mainInput.value;
+		mainInput.value = "";
+	}
+
+	else if (sum === ".") {
+		mainInput.value = "0.";
+	}
+	for (let i = 1; i < mainInput.value.length; i++) {
+		if (mainInput.value[i] === "-") {
+			fakeInput.value += mainInput.value;
+			mainInput.value = "";
+		}
+	}
+};
 
 const clean = () => {
-	document.calculator.textview.value = "";
-}
+	mainInput.value = "";
+};
+
+
 const equally = () => {
-	let result = document.calculator.textview.value;
+	let result = fakeInput.value + mainInput.value;
 	if (result) {
-		document.calculator.textview.value = eval(result);
+		result = result.replace("--", "+");
+		mainInput.value = eval(result);
 	}
-}
+	fakeInput.value = "";
+};
+
+const deleteLastSymbol = () => {
+	mainInput.value = mainInput.value.substr(0, mainInput.value.length - 1);
+};
 
 
-document.addEventListener('keydown', function () { });
+document.addEventListener("keydown", (event) => {
+	const values = [
+		"1",
+		"2",
+		"3",
+		"4",
+		"5",
+		"6",
+		"7",
+		"8",
+		"9",
+		"0",
+		"*",
+		"/",
+		"+",
+		"-",
+		".",
+	];
+	if (values.includes(event.key)) {
+		insert(event.key)
+	}
+	else if (event.key === "Backspace") {
+		deleteLastSymbol();
+	}
+	else if (event.key === "Enter") {
+		event.preventDefault()
+		equally()
+	}
+});
